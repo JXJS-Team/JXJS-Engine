6db4296436ded0bfaf15fc792a8a383439768b8e
XuelDev <103183001+XuelDev@users.noreply.github.com>
2022-11-03 14:46:48 +0000

    Announcements Now Work

--- source/states/AnnouncementState.hx
+++ source/states/AnnouncementState.hx
@@ -16,20 +16,21 @@ class AnnouncementState extends FlxState {
     public var announcementText:FlxText;
 
     override public function create() {
-        background = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+        background = new FlxSprite().loadGraphic(Paths.image('menu/menuDesat'));
         add(background);
 
         var http = new haxe.Http("https://raw.githubusercontent.com/JXJS-Team/JXJS-Engine/main/AnnouncerStuff.txt");
-        var returnedData:Array<String> = [];
 
         http.onData = function(data:String) {
             if (data == "") {
-                announcementText = new FlxText(0,0,0,"There are no new announcements.",16);
+                announcementText = new FlxText(0,0,0,"There are no new announcements. \n Press SPACE to recheck.",30);
+                announcementText.color = FlxColor.BLACK;
                 announcementText.screenCenter();
                 add(announcementText);
             } else {
                 announcementText = new FlxText(0,0,0,data,16);
                 announcementText.screenCenter();
+                announcementText.color = FlxColor.BLACK;
                 add(announcementText);
             }
 
@@ -45,5 +46,36 @@ class AnnouncementState extends FlxState {
         }
 
         http.request();
+    }
+
+    override public function update(elapsed) {
+        if (FlxG.keys.justPressed.SPACE) {
+            remove(announcementText);
+            var http = new haxe.Http("https://raw.githubusercontent.com/JXJS-Team/JXJS-Engine/main/AnnouncerStuff.txt");
+
+            http.onData = function(data:String) {
+                if (data == "") {
+                    announcementText = new FlxText(0,0,0,"There are no new announcements. \n Press SPACE to recheck.",30);
+                    announcementText.color = FlxColor.BLACK;
+                    announcementText.screenCenter();
+                    add(announcementText);
+                } else {
+                    announcementText = new FlxText(0,0,0,data,16);
+                    announcementText.screenCenter();
+                    announcementText.color = FlxColor.BLACK;
+                    add(announcementText);
+                }
+    
+    
+            }
+    
+            http.request();
+        }
+
+        if (FlxG.keys.justPressed.ESCAPE) {
+            FlxG.camera.fade(FlxColor.BLACK, 0.6, false, function() {
+                FlxG.switchState(new states.MainMenuState());
+            });
+        }
     }
 }
